7ae13324a5ff92b468c36570f8b5e33f83284dae
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-07 15:20:38 +0300

    Update App.css, App.jsx, and NoteBox.jsx. - Add CSS styles for the sticky notes app in App.css. - Add functionality for creating and displaying sticky notes in App.jsx. - Add a component for creating new sticky notes in NoteBox.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       },
     },
   ]);
-  const [position, setPosition] = useState({
+  const [mousePosition, setMousePosition] = useState({
     x: 0,
     y: 0,
   });
@@ -42,15 +42,15 @@ function App() {
   };
 
   const handleMouseMove = (e) => {
-    setPosition({ x: e.pageX, y: e.pageY });
+    setMousePosition({ x: e.pageX, y: e.pageY });
   };
 
   const handleClick = () => {
     if (!mode) return;
 
     setBoxPostion({
-      x: position.x,
-      y: position.y,
+      x: mousePosition.x,
+      y: mousePosition.y,
     });
     setBoxVisible(true);
   };
@@ -68,15 +68,21 @@ function App() {
         src="https://images.unsplash.com/photo-1550475966-70af27831597?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       />
-      {mode && <MouseFollowerText position={position} />}
-
-      {mode && <div>Comment Mode</div>}
+      {mode && <MouseFollowerText position={mousePosition} />}
 
       {notes.map((note) => {
         return <Note key={note.id} note={note} />;
       })}
 
-      {boxVisible && <NoteBox boxPostion={boxPostion} />}
+      {boxVisible && (
+        <NoteBox
+          boxPostion={boxPostion}
+          setMode={setMode}
+          notes={notes}
+          setNotes={setNotes}
+          setBoxVisible={setBoxVisible}
+        />
+      )}
     </div>
   );
 }
--- src/components/NoteBox.jsx
+++ src/components/NoteBox.jsx
@@ -1,37 +1,76 @@
-function NoteBox({ boxPostion }) {
-  const noteTypes = [
-    {
-      name: "comment",
-      color: "red",
-      text: "Comment",
-    },
-    {
-      name: "private-comment",
-      color: "#999",
-      text: "Private Comment",
-    },
-    {
-      name: "note",
-      color: "orange",
-      text: "Note",
-    },
-  ];
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+const noteTypes = [
+  {
+    name: "comment",
+    color: "purple",
+    text: "Comment",
+  },
+  {
+    name: "private-comment",
+    color: "#999",
+    text: "Private Comment",
+  },
+  {
+    name: "note",
+    color: "orange",
+    text: "Note",
+  },
+];
+function NoteBox({ boxPostion, setMode, notes, setNotes, setBoxVisible }) {
+  const [color, setColor] = useState(noteTypes[0].color);
+  const [note, setNote] = useState("");
+
+  const changeColor = (e) => {
+    setColor(e.target.value);
+  };
+
+  const addNote = () => {
+    if (!note) return;
+    const newNote = {
+      id: nanoid(),
+      note,
+      color: color,
+      position: {
+        x: boxPostion.x,
+        y: boxPostion.y,
+      },
+    };
+
+    setNotes([...notes, newNote]);
+    setNote("");
+    setBoxVisible(false);
+    setMode(true);
+  };
+
   return (
     <div
+      onMouseEnter={() => setMode(false)}
+      onMouseLeave={() => setMode(true)}
       className="note-box"
       style={{
+        "--color": color,
         position: "absolute",
-        top: boxPostion.y,
-        left: boxPostion.x + 15,
+        top: boxPostion.y + 10,
+        left: boxPostion.x + 20,
       }}
     >
-      <select>
+      <span className="note-box-number">{notes.length + 1}</span>
+      <select onChange={changeColor}>
         {noteTypes.map((type, index) => (
-          <option key={index} value={type.name}>
+          <option key={index} value={type.color}>
             {type.text}
           </option>
         ))}
       </select>
+      <textarea
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
+        placeholder="Enter note"
+        rows="10"
+      ></textarea>
+      <button onClick={addNote}>Ekle</button>
     </div>
   );
 }
