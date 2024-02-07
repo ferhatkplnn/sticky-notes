08224c875f0864d4a1d8e7c52432dc9796bfd5d7
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-07 10:54:47 +0300

    Added a comment mode toggle feature. In comment mode, a div with the text "Comment Mode" is displayed. The mode is toggled by pressing the "c" key.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useRef } from "react";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
 function App() {
-  return <div>App</div>;
+  const screen = useRef(null);
+  const [mode, setMode] = useState(false);
+
+  useEffect(() => {
+    screen.current.focus();
+  }, []);
+
+  const handleKeyUp = (e) => {
+    if (e.key === "c") {
+      console.log("comment mode active");
+      setMode(!mode);
+    }
+  };
+  return (
+    <div ref={screen} tabIndex={0} className="screen" onKeyUp={handleKeyUp}>
+      {mode && <div>Comment Mode</div>}
+    </div>
+  );
 }
 
 export default App;
